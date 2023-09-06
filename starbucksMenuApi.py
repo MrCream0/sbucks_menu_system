af06cd10aac47416bfe0f9d9b8b6bf4bc449ac94
MrCream0 <slamboi666@gmail.com>
2023-09-06 12:36:48 -0500

    Ive added more drink options and changed the item detail and sub category insertions to take a varaible of concatenated lists of tuples

--- starbucksMenuApi.py
+++ starbucksMenuApi.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-# Create or connect to the database
 connection = sqlite3.connect('starbucks_menu.db')
 cursor = connection.cursor()
 
-# Create tables for menu categories, items, and item details
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS categories (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -108,10 +106,9 @@ menu_drink_hot_coffee_subcategory = [
     (7, "Macchiatos", "hot-coffee"),
     (8, "Mochas", "hot-coffee"),
     (9, "Travelers", "hot-coffee"),
-    # Add more items as needed
 ]
 
-menu_drink_hotcoffee = [
+menu_drink_hot_coffee = [
     (1, "Caffe Americano", 0.00, "enter desc", "hot-coffee"),
     (2, "Featured Blonde Roast", 0.00, "enter desc", "hot-coffee"),
     (2, "Featured Medium Roast Pike Place Roast", 0.00, "enter desc", "hot-coffee"),
@@ -149,24 +146,178 @@ menu_drink_hotcoffee = [
     (9, "Decaf Pike", 0.00, "enter desc", "hot-coffee"),
 ]
 
+menu_drink_hot_tea_subcategory = [
+    (1, "Chai Teas", "hot-tea"),
+    (2, "Black Teas", "hot-tea"),
+    (3, "Green Teas", "hot-tea"),
+    (4, "Herbal Teas", "hot-tea"),
+]
 
-# Insert menu item details (prices and descriptions)
+menu_drink_hot_tea = [
+    (1, "Chai Tea Latte", 0.00, "enter desc", "hot-tea"),
+    (1, "Chai Tea", 0.00, "enter desc", "hot-tea"),
+    (2, "Earl Grey Tea", 0.00, "enter desc", "hot-tea"),
+    (2, "Teavana London Fog Tea Latte", 0.00, "enter desc", "hot-tea"),
+    (2, "Royal English Breakfast Tea", 0.00, "enter desc", "hot-tea"),
+    (2, "Royal English Breakfast Tea Latte", 0.00, "enter desc", "hot-tea"),
+    (3, "Emperors Clouds & Mist", 0.00, "enter desc", "hot-tea"),
+    (3, "Matcha Tea Latte", 0.00, "enter desc", "hot-tea"),
+    (3, "Honey Citrus Mint Tea", 0.00, "enter desc", "hot-tea"),
+    (3, "Jade Citrus Mint Brewed Tea", 0.00, "enter desc", "hot-tea"),
+    (4, "Mint Majesty", 0.00, "enter desc", "hot-tea"),
+    (4, "Peach Tranquility", 0.00, "enter desc", "hot-tea"),
+]
+
+menu_drink_hot_drink_subcategory = [
+    (1, "Hot Chocolates", "hot-drink"),
+    (2, "Juice", "hot-drink"),
+    (3, "Steamers", "hot-drink"),
+]
+
+menu_drink_hot_drink = [
+    (1, "Hot Chocolate", 0.00, "enter desc", "hot-drink"),
+    (1, "White Hot Chocolate", 0.00, "enter desc", "hot-drink"),
+    (2, "Caramel Apple Spice", 0.00, "enter desc", "hot-drink"),
+    (2, "Steamed Apple Juice", 0.00, "enter desc", "hot-drink"),
+    (3, "Pumpkin Spice Creme", 0.00, "enter desc", "hot-drink"),
+    (3, "Steamed Milk", 0.00, "enter desc", "hot-drink"),
+    (3, "Vanilla Creme", 0.00, "enter desc", "hot-drink"),
+]
+
+menu_drink_frappuccino_subcategory = [
+    (1, "Coffee Frappuccino", "frappuccino-drink"),
+    (2, "Creme Frappuccino", "frappuccino-drink"),
+]
+
+menu_drink_frappuccino_drink = [
+    (1, "Apple Crisp Oatmilk Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Pumpkin Spice Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Mocha Cookie Crumble Frappuccino", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Caramel Ribbon Crunch Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Espresso Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Caffe Vanilla Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Caramel Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Coffee Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Mocha Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "Java Chip Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (1, "White Chocolate Mocha Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Apple Crisp Oatmilk Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Pumpkin Spice Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Chocolate Cookie Crumble Creme Frappuccino", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Caramel Ribbon Crunch Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Strawberry Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Chai Creme Frappuccino", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Double Chocolaty Chip Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Matcha Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "Vanilla Bean Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+    (2, "White Chocolate Creme Frappuccino Blended Beverage", 0.00, "enter desc", "frappuccino-drink"),
+]
+
+menu_drink_cold_coffee_subcategory = [
+    (1, "Cold Brews", "cold-coffee"),
+    (2, "Nitro Cold Brews", "cold-coffee"),
+    (3, "Iced Americano", "cold-coffee"),
+    (4, "Iced Coffees", "cold-coffee"),
+    (5, "Iced Shaken Espresso", "cold-coffee"),
+    (6, "Iced Flat Whites", "cold-coffee"),
+    (7, "Iced Lattes", "cold-coffee"),
+    (8, "Iced Macchiatos", "cold-coffee"),
+    (9, "Iced Mochas", "cold-coffee"),
+]
+
+menu_drink_cold_coffee = [
+    (1, "Pumpkin Cream Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (1, "Oleato Golden Foam Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (1, "Cinnamon Caramel Cream Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (1, "Chocolate Cream Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (1, "Salted Caramel Cream Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (1, "Starbucks reserve Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (1, "Starbucks Cold Brew Coffee", 0.00, "enter desc", "cold-coffee"),
+    (1, "Vanilla Sweet Cream Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (1, "Starbucks Cold Brea Coffee with Milk", 0.00, "enter desc", "cold-coffee"),
+    (2, "Cinnamon Caramel Cream Nitro Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (2, "Nitro Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (2, "Vanilla Sweet Cream Nitro Cold Brew", 0.00, "enter desc", "cold-coffee"),
+    (3, "Iced Caffe Amaricano", 0.00, "enter desc", "cold-coffee"),
+    (4, "Iced Coffee", 0.00, "enter desc", "cold-coffee"),
+    (4, "Iced Coffee Iced Coffee with Milk", 0.00, "enter desc", "cold-coffee"),
+    (4, "Iced Espresso", 0.00, "enter desc", "cold-coffee"),
+    (5, "Iced Apple Crisp Oatmilk Shaken Espresso", 0.00, "enter desc", "cold-coffee"),
+    (5, "Oleato Iced Shaken Espresso with Oatmilk and Toffeenut", 0.00, "enter desc", "cold-coffee"),
+    (5, "Iced Toasted Vanilla Oatmilk Shaken Espresso", 0.00, "enter desc", "cold-coffee"),
+    (5, "Iced Brown Sugar Oatmilk Shaken Espresso", 0.00, "enter desc", "cold-coffee"),
+    (5, "Iced Chocolate Almondmilk Shaken Espresso", 0.00, "enter desc", "cold-coffee"),
+    (5, "Iced Shaken Espresso", 0.00, "enter desc", "cold-coffee"),
+    (6, "Iced Flat White", 0.00, "enter desc", "cold-coffee"),
+    (6, "Iced Honey Almondmilk Flat White", 0.00, "enter desc", "cold-coffee"),
+    (7, "Iced Pumpkin Spice Latte", 0.00, "enter desc", "cold-coffee"),
+    (7, "Starbucks Reserve Iced Latte", 0.00, "enter desc", "cold-coffee"),
+    (7, "Starbucks Reserve Iced Hazlenut Bianco Latte", 0.00, "enter desc", "cold-coffee"),
+    (7, "Iced Caffe Latte", 0.00, "enter desc", "cold-coffee"),
+    (7, "Iced Cinnamon Dolce Latte", 0.00, "enter desc", "cold-coffee"),
+    (7, "Iced Starbucks Blonde Vanilla Latte", 0.00, "enter desc", "cold-coffee"),
+    (8, "Iced Apple Crisp Oatmilk Macchiato", 0.00, "enter desc", "cold-coffee"),
+    (8, "Iced Caramel Macchiato", 0.00, "enter desc", "cold-coffee"),
+    (9, "Iced White Chocolate Mocha", 0.00, "enter desc", "cold-coffee"),
+    (9, "Iced Caffe Mocha", 0.00, "enter desc", "cold-coffee"),
+    (9, "Starbucks Reserve Iced Dark Chocolate Mocha", 0.00, "enter desc", "cold-coffee"),
+]
+
+menu_drink_iced_tea_subcategory = [
+    (1, "Bottled Teas", "iced-tea"),
+    (2, "Iced Black Teas", "iced-tea"),
+    (3, "Iced Chai Teas", "iced-tea"),
+    (4, "Iced Green Teas", "iced-tea"),
+    (5, "Iced Herbal Teas", "iced-tea"),
+]
+
+menu_drink_iced_tea = [
+    (1, "Teavana Sparkling Unsweetened Peach Nectarine Green Tea", 0.00, "enter desc", "iced-tea"),
+    (1, "Teavana Mango Black Tea", 0.00, "enter desc", "iced-tea"),
+    (2, "Iced Black Tea", 0.00, "enter desc", "iced-tea"),
+    (2, "Iced Black Tea Lemonade", 0.00, "enter desc", "iced-tea"),
+    (2, "Iced Royal English Breakfast Tea Latte", 0.00, "enter desc", "iced-tea"),
+    (2, "Iced London Fog Tea Latte", 0.00, "enter desc", "iced-tea"),
+    (3, "Iced Pumpkin Cream Chai Tea Latte", 0.00, "enter desc", "iced-tea"),
+    (3, "Iced Chai Tea Latte", 0.00, "enter desc", "iced-tea"),
+    (4, "Iced Peach Green Tea", 0.00, "enter desc", "iced-tea"),
+    (4, "Iced Peach Green Tea Lemonade", 0.00, "enter desc", "iced-tea"),
+    (4, "Iced Matcha Tea Latte", 0.00, "enter desc", "iced-tea"),
+    (4, "Iced Green Tea", 0.00, "enter desc", "iced-tea"),
+    (4, "Iced Green Tea Lemonade", 0.00, "enter desc", "iced-tea"),
+    (4, "Iced Matcha Lemonade", 0.00, "enter desc", "iced-tea"),
+    (5, "Iced Passion Tango Tea", 0.00, "enter desc", "iced-tea"),
+    (5, "Iced Passion Tango Tea Lemonade", 0.00, "enter desc", "iced-tea"),
+
+]
+
+menu_items =\
+    menu_drink_oleato +\
+    menu_drink_hot_coffee +\
+    menu_drink_hot_tea +\
+    menu_drink_hot_drink +\
+    menu_drink_frappuccino_drink +\
+    menu_drink_cold_coffee +\
+    menu_drink_iced_tea
+
+menu_items_sub =\
+    menu_drink_hot_coffee_subcategory +\
+    menu_drink_hot_tea_subcategory +\
+    menu_drink_hot_drink_subcategory +\
+    menu_drink_frappuccino_subcategory +\
+    menu_drink_cold_coffee_subcategory +\
+    menu_drink_iced_tea_subcategory
+
 cursor.executemany('''
     INSERT INTO menu_item_details (item_id, name, price, description, subcategory)
     VALUES (?, ?, ?, ?, ?)
-''', menu_drink_oleato)
-
-cursor.executemany('''
-    INSERT INTO menu_item_details (item_id, name, price, description, subcategory)
-    VALUES (?, ?, ?, ?, ?)
-''', menu_drink_hotcoffee)
+''', menu_items)
 
 cursor.executemany('''
     INSERT INTO menu_item_subcategories (item_id, name, subcategory)
     VALUES (?, ?, ?)
-''', menu_drink_hot_coffee_subcategory)
+''', menu_items_sub)
 
-# Commit the changes and close the connection
 connection.commit()
 connection.close()
 
