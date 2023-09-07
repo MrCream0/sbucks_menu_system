3b3c4e1b0d494208f8e8847480e033c4a2dc3da7
MrCream0 <slamboi666@gmail.com>
2023-09-07 00:23:34 -0500

    Changed the API currently able to get the menu categories all drink options and specific items by item id

--- starbucksMenuAPI.py
+++ starbucksMenuAPI.py
@@ -1,69 +1,55 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/menu-category-items", methods=["GET"])
-def get_menu_details():
-    # Connect to the SQLite database
-    connection = sqlite3.connect("starbucks_menu.db")
-    cursor = connection.cursor()
-
-    # Query the database to retrieve menu data
-    cursor.execute("SELECT * FROM menu_item_details")
-    menu_items = cursor.fetchall()
-
-    # Close the database connection
-    connection.close()
-
-    # Convert the menu data to JSON and return it
-    menu_json = [{"id": row[0],
-                  "item_id": row[1],
-                  "name": row[2],
-                  "price": row[3],
-                  "Description": row[4],
-                  "subcategory": row[5]} for row in menu_items]
-    return jsonify(menu_json)
+def get_db_connection():
+    connection = sqlite3.connect('starbucks_menu.db')
+    connection.row_factory = sqlite3.Row
+    return connection
 
 
-@app.route("/menu-category", methods=["GET"])
-def get_menu_category():
-    # Connect to the SQLite database
-    connection = sqlite3.connect("starbucks_menu.db")
-    cursor = connection.cursor()
-
-    # Query the database to retrieve menu data
-    cursor.execute("SELECT * FROM menu_category_items")
-    menu_items = cursor.fetchall()
-
-    # Close the database connection
-    connection.close()
-
-    # Convert the menu data to JSON and return it
-    menu_json = [{"id": row[0],
-                  "category_id": row[1],
-                  "subcategory": row[2]} for row in menu_items]
-    return jsonify(menu_json)
+def execute_query(query, args=()):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, args)
+    conn.commit()
+    conn.close()
 
 
-@app.route("/menu", methods=["GET"])
+@app.route('/categories', methods=['GET'])
+def get_categories():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM categories')
+    categories = cursor.fetchall()
+    conn.close()
+    return jsonify([dict(category) for category in categories])
+
+
+@app.route('/menu-items', methods=['GET'])
 def get_menu_items():
-    # Connect to the SQLite database
-    connection = sqlite3.connect("starbucks_menu.db")
-    cursor = connection.cursor()
-
-    # Query the database to retrieve menu data
-    cursor.execute("SELECT * FROM categories")
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM menu_item_details')
     menu_items = cursor.fetchall()
-
-    # Close the database connection
-    connection.close()
-
-    # Convert the menu data to JSON and return it
-    menu_json = [{"name": row[1]} for row in menu_items]
-    return jsonify(menu_json)
+    conn.close()
+    return jsonify([dict(item) for item in menu_items])
 
 
-if __name__ == "__main__":
+@app.route('/menu-items/<int:item_id>', methods=['GET'])
+def get_menu_item(item_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM menu_item_details WHERE id = ?', (item_id,))
+    menu_item = cursor.fetchone()
+    conn.close()
+    if menu_item:
+        return jsonify(dict(menu_item))
+    else:
+        return jsonify({"message": "Menu item not found"}), 404
+
+
+if __name__ == '__main__':
     app.run(debug=True)
